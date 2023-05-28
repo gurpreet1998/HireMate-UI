8d5bbec3555b34fe77d385a4f0247cd1602f70b4
Gurpreet <37729999+gurpreet1998@users.noreply.github.com>
2023-05-28 03:45:46 -0700

    Resolved CORS Issue

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,6 +3,11 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL: "http://20.127.253.108/api/",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+    "Access-Control-Allow-Methods": "GET, POST", // Allow GET and POST methods
+    "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow specific headers
+  },
 });
 
 export default newRequest;
